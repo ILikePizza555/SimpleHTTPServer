b1236bc03035f7e1b547148c3c541b63e01f2322
Isaac Avram <avrisaac555@gmail.com>
2018-12-06 20:16:45 -0800

    Moved all functions into the utils namespace

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,90 +1,97 @@
 #include "StringUtils.h"
 
-std::vector<std::string> utils::string::split(std::string string, std::string delim, int count)
+namespace utils
 {
-	std::vector<std::string> rv;
-
-	int n = string.find(delim);
-
-	while (n != std::string::npos && count != 0)
+	std::vector<std::string> split(std::string string, std::string delim, int count)
 	{
-		//Add the first part of the string to the vector
-		rv.push_back(string.substr(0, n));
-		//Erase the cut part (we include the delimnator this time
-		string.erase(0, n + delim.length());
+		std::vector<std::string> rv;
 
-		//Set the variables
-		n = string.find(delim);
-		count--;
+		int n = string.find(delim);
+
+		while (n != std::string::npos && count != 0) {
+			//Add the first part of the string to the vector
+			rv.push_back(string.substr(0, n));
+			//Erase the cut part (we include the delimnator this time
+			string.erase(0, n + delim.length());
+
+			//Set the variables
+			n = string.find(delim);
+			count--;
+		}
+
+		//Add the rest of the string to the vector, if not empty
+		if (!string.empty()) rv.push_back(string);
+
+		return rv;
 	}
 
-	//Add the rest of the string to the vector, if not empty
-	if (!string.empty()) rv.push_back(string);
-
-	return rv;
-}
-
-std::string utils::string::concat(std::initializer_list<std::string> args, std::string delim, int beginOffset, int endOffset)
-{
-	std::ostringstream os;
-	//Std::copy expects a pointer to 1 past the last element of an array. So, by subtracting one here, it copies all but the last.
-	std::copy(args.begin() + beginOffset, args.end() - endOffset, std::ostream_iterator<std::string>(os, delim.c_str()));
-	//args.end() returns a pointer to 1 past the last element of an array. So, we need to subtract one.
-	os << *(args.end() - 1);
-
-	return os.str();
-}
-
-std::string utils::string::concat(std::vector<std::string> args, std::string delim, int beginOffset, int endOffset)
-{
-	std::ostringstream os;
-	std::copy(args.begin() + beginOffset, args.end() - endOffset, std::ostream_iterator<std::string>(os, delim.c_str()));
-	os << args.back();
-
-	return os.str();
-}
-
-void utils::string::ltrim(std::string& string)
-{
-	string.erase(string.begin(), std::find_if(string.begin(), string.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-}
-
-void utils::string::rtrim(std::string& string)
-{
-	string.erase(std::find_if(string.rbegin(), string.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), string.end());
-}
-
-void utils::string::trim(std::string& string)
-{
-	ltrim(string);
-	rtrim(string);
-}
-
-bool utils::string::beginsWith(std::string str, std::string begin)
-{
-	return str.substr(0, begin.length()) == begin;
-}
-
-bool utils::string::endsWith(std::string str, std::string end)
-{
-	int beginIndex = str.length() - end.length();
-	return str.substr(beginIndex, end.length()) == end;
-}
-
-void utils::string::replace(std::string& src, std::string sub, std::string replace, int count)
-{
-	while (count != 0)
+	std::string
+	concat(std::initializer_list<std::string> args, std::string delim, int beginOffset, int endOffset)
 	{
-		size_t index = 0;
-		///Find the substring
-		index = src.find(sub, index);
-		if (index == std::string::npos) break;
+		std::ostringstream os;
+		//Std::copy expects a pointer to 1 past the last element of an array. So, by subtracting one here, it copies all but the last.
+		std::copy(args.begin() + beginOffset, args.end() - endOffset,
+				  std::ostream_iterator<std::string>(os, delim.c_str()));
+		//args.end() returns a pointer to 1 past the last element of an array. So, we need to subtract one.
+		os << *(args.end() - 1);
 
-		//Replace
-		src.replace(index, sub.length(), replace);
+		return os.str();
+	}
 
-		//Move the variables
-		index += sub.length();
-		count--;
+	std::string concat(std::vector<std::string> args, std::string delim, int beginOffset, int endOffset)
+	{
+		std::ostringstream os;
+		std::copy(args.begin() + beginOffset, args.end() - endOffset,
+				  std::ostream_iterator<std::string>(os, delim.c_str()));
+		os << args.back();
+
+		return os.str();
+	}
+
+	void ltrim(std::string &string)
+	{
+		string.erase(string.begin(),
+					 std::find_if(string.begin(), string.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	}
+
+	void rtrim(std::string &string)
+	{
+		string.erase(
+				std::find_if(string.rbegin(), string.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
+				string.end());
+	}
+
+	void trim(std::string &string)
+	{
+		ltrim(string);
+		rtrim(string);
+	}
+
+	bool beginsWith(std::string str, std::string begin)
+	{
+		return str.substr(0, begin.length()) == begin;
+	}
+
+	bool endsWith(std::string str, std::string end)
+	{
+		int beginIndex = str.length() - end.length();
+		return str.substr(beginIndex, end.length()) == end;
+	}
+
+	void replace(std::string &src, std::string sub, std::string replace, int count)
+	{
+		while (count != 0) {
+			size_t index = 0;
+			///Find the substring
+			index = src.find(sub, index);
+			if (index == std::string::npos) break;
+
+			//Replace
+			src.replace(index, sub.length(), replace);
+
+			//Move the variables
+			index += sub.length();
+			count--;
+		}
 	}
 }
--- src/StringUtils.h
+++ src/StringUtils.h
@@ -11,33 +11,38 @@
 
 namespace utils
 {
-	namespace string
+
+	std::vector<std::string> split(std::string string, std::string delim, int count = -1);
+
+	std::string
+	concat(std::initializer_list<std::string> args, std::string delim = "", int beginOffset = 0, int endOffset = 1);
+
+	std::string concat(std::vector<std::string> args, std::string delim = "", int beginOffset = 0, int endOffset = 1);
+
+	void ltrim(std::string &string);
+
+	void rtrim(std::string &string);
+
+	void trim(std::string &string);
+
+	bool beginsWith(std::string str, std::string begin);
+
+	bool endsWith(std::string str, std::string end);
+
+	void replace(std::string &src, std::string sub, std::string replace, int count = -1);
+
+	std::vector<std::string> split(std::string string, const std::string &delim, int count)
 	{
-		std::vector<std::string> split(std::string string, std::string delim, int count = -1);
+		std::vector<std::string> rv;
 
-		std::string concat(std::initializer_list<std::string> args, std::string delim = "", int beginOffset = 0, int endOffset = 1);
-		std::string concat(std::vector<std::string> args, std::string delim = "", int beginOffset = 0, int endOffset = 1);
-
-		void ltrim(std::string& string);
-		void rtrim(std::string& string);
-		void trim(std::string& string);
-
-		bool beginsWith(std::string str, std::string begin);
-		bool endsWith(std::string str, std::string end);
-
-		void replace(std::string& src, std::string sub, std::string replace, int count = -1);
-
-		std::vector<std::string> split(std::string string, const std::string& delim, int count) {
-			std::vector<std::string> rv;
-
-			while (count != 0) {
+		while (count != 0) {
 
 
-				int i = string.find(delim);
-				rv.push_back(string.substr(0, i));
-				string = string.substr(i);
-				count--;
-			}
+			int i = string.find(delim);
+			rv.push_back(string.substr(0, i));
+			string = string.substr(i);
+			count--;
 		}
 	}
+
 }
